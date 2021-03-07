777078f476041ef0e4fcf17b33c81e44250d3334
Ciro <Ciro16@gmail.com>
2021-03-07 10:38:07 -0800

    added handleSubmit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,72 @@
-import React, {Component} from 'react';
-import FormDetails from './FormDetails'
+import React, { useState} from 'react';
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
-export class Form extends Component {
-    state = {
-        name: '',
-        email: '',
-        message: ''
-    }
-
-
-handleChange = input => e => {
-    this.setState({ [input]: e.target.value })
+let myStyle = {
+    "textAlign": "center",
+    "marginTop": "100px"
 }
 
-render() {
-    const { name, email, message } = this.state
-    const values = { name, email, message }
+
+
+export function Form() {
+
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        let response = {
+            "name": name,
+            "email": email,
+            "message": message
+        }
+
+        const requestOptions = { 
+                method: 'POST', 
+                headers: { 'Content-Type': 'application/json' }, 
+                body: JSON.stringify(response) 
+            }; 
+                fetch('https://76y592jsbi.execute-api.us-west-1.amazonaws.com/dev', requestOptions)
+                    .then(response => response.json())
+    }        
+
         return (
-            <FormDetails
-                handleChange={this.handleChange}
-                values={values}
-            />
+            <div style={myStyle}>
+                <h1>Email Form for MK Decision</h1>
+                <form method="POST"
+                    onSubmit={handleSubmit}>
+                    <TextField 
+                        required 
+                        label="Name" 
+                        value={name}
+                        onInput={e => setName(e.target.value)}
+                    />
+                    <br />
+                    <TextField 
+                        required 
+                        type="email" 
+                        label="Email" 
+                        value={email}
+                        onInput={e => setEmail(e.target.value)}
+                    />
+                    <br />
+                    <TextField 
+                        required 
+                        label="Message" 
+                        multiline 
+                        rows={2} 
+                        rowsMax={4} 
+                        value={message} 
+                        onInput={e => setMessage(e.target.value)}
+                    />
+                    <br />
+                    <br />
+                    <Button type="submit" variant="contained" color="primary">
+                        Submit
+                    </Button>
+                </form>
+            </div>
         );
-    }
 }
